7afb1911dffc4401f4bf2904cc36608a3f9002ce
ToKProK <i@DESKTOP-6G6BLV1>
2024-03-12 19:20:44 +0300

    new fitchers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+import webbrowser
 
 Mafia = telebot.TeleBot('6847605581:AAFshf6F811PHzdMu98gvEg26RRNOrLf2Y8')
 
@@ -7,14 +8,22 @@ Mafia = telebot.TeleBot('6847605581:AAFshf6F811PHzdMu98gvEg26RRNOrLf2Y8')
 def main(message):
     Mafia.send_message(message.chat.id, 'Привет!')
 
+@Mafia.message_handler(commands=["open_new_bot"])
+def new_bot_open(message):
+    webbrowser.open("https://t.me/Punching_phone_bot")
 
 @Mafia.message_handler(func=lambda message: message.text.lower() == 'hello')
 def info(message):
     Mafia.send_message(message.chat.id, 'Hello')
 
+@Mafia.message_handler(commands=["get_my_name"])# Команда, которая демонстрирует возможность обращения к пользоваелю по имени
+def info(message):
+    Mafia.send_message(message.chat.id, f'Привет: {message.from_user.first_name} {message.from_user.last_name}')
 
-@Mafia.message_handler(func=lambda message: message.text.lower() == 'idd')
+
+@Mafia.message_handler(func=lambda message: message.text.lower() == 'id')
 def idd(message):
     Mafia.send_message(message.chat.id, 'idd')
+
 
 Mafia.polling(none_stop=True)
